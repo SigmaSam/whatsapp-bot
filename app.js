314c58df7fc97a4eea5927be0cbb3a7152aa4dda
samuel <samuelalmaoherrera@gmail.com>
2021-07-04 17:45:47 -0300

    Add historial fucionality and media sending.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const ora = require('ora');
+const ExcelJS = require('exceljs');
+const moment = require('moment')
 const chalk = require('chalk');
-const { Client } = require('whatsapp-web.js');
+const { Client, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');  
 
 const SESSION_FILE_PATH = './session.json';
@@ -52,7 +54,7 @@ const withOutSession = () => {
     client.initialize();
 }
 
-/*Message Listener */
+/* Message Listener */
 
 const listenMessage = () => {
     client.on('message', (msg)=> {
@@ -61,15 +63,66 @@ const listenMessage = () => {
         /* User Input */
         switch(body) {
             case 'Hola':
-                sendMessage(from, 'Hola somos Baumes, como podemos ayudarte?\n Para ver nuestro catalogo escribe *Catalogo*.\n Para atencion personal escribe *Che guacho dame bola*'); 
+                sendMessage(from, 'Hola somos Baumes, como podemos ayudarte?\n Para ver nuestro catalogo escribe *Catalogo*.\n Para atencion personal escribe *Che guacho dame bola*');
+                break;
+            case 'Catalogo': 
+                sendMessage(from, 'Un pdf por probar.');
+                sendMedia(from, 'herothousandfaces.pdf');
         }
-       
+       saveHistorial(from, body)
     })
 }
 
-/*Message Sender */
+/* Message Sender */
 const sendMessage = (to, message) => {
     client.sendMessage(to, message)
+}
+
+/* Send media */
+const sendMedia = (to, file) => {
+    const mediaFile = MessageMedia.fromFilePath(`./Media/${file}`);
+    client.sendMessage(to, mediaFile)
+}
+
+/* Save Chats */
+const saveHistorial = (number, message) => {
+    const pathChat = `./Chats/${number}.xlsx`
+    const workbook = new ExcelJS.Workbook();
+    const today = moment().format('DD-MM-YYYY hh:mm');
+
+    if(fs.existsSync(pathChat)) {
+        workbook.xlsx.readFile(pathChat)
+        .then(()=> {
+            const worksheet = workbook.getWorksheet(1);
+            const lastRow = worksheet.lastRow;
+            let getRowInsert = worksheet.getRow(++(lastRow.number));
+            getRowInsert.getCell('A').value = today;
+            getRowInsert.getCell('B').value = message;
+            getRowInsert.commit();
+            workbook.xlsx.writeFile(pathChat)
+            .then(() => {
+                console.log('Chat Updated');
+            })
+            .catch(() => {
+                console.log('Error Updating Chat.')
+            })
+
+        })
+    } else {
+        const worksheet = workbook.addWorksheet('Chats');
+        worksheet.columns = [
+            { header: 'Date', key: 'date' },
+            { header: 'Message', key: 'message' },
+        ];
+        worksheet.addRow([today, message]);
+        workbook.xlsx.writeFile(pathChat)
+            .then(() => {
+                console.log(' Added to History.');
+            })
+            .catch(() => {
+                console.log(' Error Creating History.');
+            })
+    }
 }
 
 (fs.existsSync(SESSION_FILE_PATH)) ? withSession() : withOutSession();
